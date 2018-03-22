6c6c8dcd333baa04e8ea5de11131a2d6e99d7144
Jeanfranco Paolo <jeanfranko.pci16@gmail.com>
2018-03-21 23:48:43 -0500

    ELiminando directorios creados por IntelliJ

---
+++ out/artifacts/Aplicacion01HTML_war_exploded/js/validacion.js
@@ -0,0 +1,13 @@
+function validacion() {
+	debugger;
+	var isbn = document.getElementById("isbn");
+	var miformulario = document.getElementById("miformulario"); 
+	
+	if (isbn.value == "") {
+		alert("datos no validos");
+		return false; 
+	}
+	else{
+		miformulario.submit();
+	} 
+}
